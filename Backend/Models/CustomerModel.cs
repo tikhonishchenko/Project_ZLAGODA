9f69fa6168fc93393c436ae90c4b9de3b303c000
Тіхон Іщенко <tikhon.ishchenko@ukma.edu.ua>
2023-03-27 23:18:27 +0300

    Added models for other entities

---
+++ Backend/Models/CustomerModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_ZLAGODA.Backend.Models
+{
+    internal class CustomerModel
+    {
+        public string CardNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public int Discount { get; set; }
+
+        public CustomerModel(string cardNumber, string firstName, string lastName, string patronymic, string phoneNumber, string address, string city, string zipCode, int discount)
+        {
+            CardNumber = cardNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            Patronymic = patronymic;
+            PhoneNumber = phoneNumber;
+            Address = address;
+            City = city;
+            ZipCode = zipCode;
+            Discount = discount;
+        }
+
+        public CustomerModel()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{CardNumber} {FirstName} {LastName} {Patronymic} {PhoneNumber} {Address} {City} {ZipCode} {Discount}";
+        }
+    }
+}
---
+++ Backend/Models/EmployeeModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_ZLAGODA.Backend.Models
+{
+    internal class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public string Role { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime DateOfEmployment { get; set; }
+        public DateTime DateOfBirth{ get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string Zip { get; set; }
+
+        public EmployeeModel(int id, string name, string surname, string patronymic, string role, string phone, decimal salary, DateTime dateOfEmployment, DateTime dateOfBirth, string city, string street, string zip)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            Role = role;
+            Phone = phone;
+            Salary = salary;
+            DateOfEmployment = dateOfEmployment;
+            DateOfBirth = dateOfBirth;
+            City = city;
+            Street = street;
+            Zip = zip;
+        }
+
+        public EmployeeModel()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name} {Surname} {Patronymic} {Role} {Phone} {Salary} {DateOfEmployment} {DateOfBirth} {City} {Street} {Zip}";
+        }
+
+
+
+    }
+}
