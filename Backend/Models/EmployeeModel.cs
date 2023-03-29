e2f30e6dd44c10ea893ec1746b85e63c008125b3
Тіхон Іщенко <tikhon.ishchenko@ukma.edu.ua>
2023-03-29 13:49:10 +0300

    додав пароль, юзернейм в працівника та їх хешування.

--- Backend/Models/EmployeeModel.cs
+++ Backend/Models/EmployeeModel.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Project_ZLAGODA.Backend.Models
 {
@@ -20,9 +25,14 @@ namespace Project_ZLAGODA.Backend.Models
         public string City { get; set; }
         public string Street { get; set; }
         public string Zip { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public string Username { get; set; }
+        public byte[] Salt { get; set; }
+        
 
-        public EmployeeModel(int id, string name, string surname, string patronymic, string role, string phone, decimal salary, DateTime dateOfEmployment, DateTime dateOfBirth, string city, string street, string zip)
+        public EmployeeModel(int id, string name, string surname, string patronymic, string role, string phone, decimal salary, DateTime dateOfEmployment, DateTime dateOfBirth, string city, string street, string zip, string password, string username)
         {
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
             Id = id;
             Name = name;
             Surname = surname;
@@ -35,12 +45,39 @@ namespace Project_ZLAGODA.Backend.Models
             City = city;
             Street = street;
             Zip = zip;
+            PasswordHash = passwordHash;
+            Username = username;
+            Salt = passwordSalt;
+
         }
 
         public EmployeeModel()
         {
         }
+        
 
+        internal bool CheckPassword(string password,byte[] passwordHash, byte[] passwordSalt)
+        {
+            return VerifyPasswordHash(password, passwordHash, passwordSalt);
+        }
+
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
         public override string ToString()
         {
             return $"{Id} {Name} {Surname} {Patronymic} {Role} {Phone} {Salary} {DateOfEmployment} {DateOfBirth} {City} {Street} {Zip}";
