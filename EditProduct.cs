e5639533c22fa914bec3d9f6ced85301ae58421d
Zeltronick <muhailo11111@gmail.com>
2023-04-07 13:17:10 +0300

    Main Window (Only products work)

---
+++ EditProduct.cs
@@ -0,0 +1,63 @@
+﻿using Project_ZLAGODA.Backend.Database;
+using Project_ZLAGODA.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Project_ZLAGODA
+{
+    public partial class EditProduct : Form
+    {
+        Form3 main;
+        public EditProduct()
+        {
+            InitializeComponent();
+        }
+
+        public void setMainForm(Form3 main)
+        {
+            this.main = main;
+        }
+
+        public void setIdTextBox(string id)
+        {
+            this.IdTextBox.Text = id;
+        }
+
+        public void setCategoryIdTextBox(string id)
+        {
+            this.CategoryIdTextBox.Text = id;
+        }
+
+        public void setNameTextBox(string name)
+        {
+            this.NameTextBox.Text = name;
+        }
+
+        public void setCharacteristicsTextBox(string characteristics)
+        {
+            this.CharacteristicsTextBox.Text = characteristics;
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            ProductModel productModel = new ProductModel
+            {
+                Id = int.Parse(IdTextBox.Text),
+                CategoryNumber = int.Parse(CategoryIdTextBox.Text),
+                ProductName = NameTextBox.Text,
+                ProductCharacteristics = CharacteristicsTextBox.Text
+            };
+            DbRepository.UpdateProduct(productModel);
+            main.ShowProductsByName();
+            this.Close();
+        }
+    }
+}
