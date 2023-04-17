d726bdee70c092d0963e65e2d8b47e5baa3527a9
zxccxccxz <galexoks@gmail.com>
2023-04-17 15:28:29 +0300

    commit and pudge

--- ViewModels/ViewModel.cs
+++ ViewModels/ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project_ZLAGODA.Backend.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
@@ -23,6 +24,33 @@ namespace Project_ZLAGODA.ViewModels
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        public static void UpdateProducts()
+        {
+            var date = DateTime.Now.AddDays(3.0);
+            var products = DbRepository.GetStoreProductsToDate(date);
+            foreach(var product in products)
+            {
+                if (product.IsPromotion || product.Quantity < 5)
+                {
+                    continue;
+                }
+                product.IsPromotion = true;
+                product.Price *= 0.8m;
+                DbRepository.UpdateStoreProduct(product);
+            }
+        }
+
+        public static void ResetPromotion()
+        {
+            var date = DateTime.Now.AddDays(3.0);
+            var products = DbRepository.GetStoreProductsToDate(date);
+            foreach (var product in products)
+            {
+                product.IsPromotion = false;
+                DbRepository.UpdateStoreProduct(product);
             }
         }
     }
